8513edc6ae1076adef79af0e8d7f37390a9719cf
Etiënne van den Tempel <e.vandentempel@ultimaker.com>
2019-01-16 10:34:42 +0100

    Rename HeroBasic-paragraph to description, move description to generic Hero

--- src/organisms/hero/Hero.ts
+++ src/organisms/hero/Hero.ts
@@ -3,7 +3,8 @@ import {CtaBlock} from "../../molecules/cta/CtaBlock";
 import {OrganismInterface} from "../OrganismInterface";
 
 export abstract class Hero implements OrganismInterface {
-    readonly ctas: CtaBlock;
     readonly image: Image;
     readonly title: string;
+    readonly description: string;
+    readonly ctas: CtaBlock;
 }
