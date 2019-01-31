1b33cdd7f23760e925472bfdeeda85b1469902d6
Michiel van Alphen <m.vanalphen@ultimaker.com>
2019-01-31 16:58:44 +0100

    Generic section (sorry)

--- src/molecules/sections/Section.ts
+++ src/molecules/sections/Section.ts
@@ -1,6 +1,9 @@
 import { Tooltip } from '../../atoms/tooltip/Tooltip';
+import { BusinessCard } from '../cards/BusinessCard';
+import { Card } from '../cards/Card';
 
 export class Section {
+    cards: BusinessCard[] | Card[];
     title?: string;
     tooltip?: Tooltip;
     limit?: number;
--- src/organisms/overview/Overview.ts
+++ src/organisms/overview/Overview.ts
@@ -1,16 +1,10 @@
 import { OrganismInterface } from '../OrganismInterface';
 import { CtaBlock } from '../../molecules/cta/CtaBlock';
-import { ResellerSection } from '../../molecules/sections/ResellerSection';
 import { Section } from '../../molecules/sections/Section';
-
-enum SectionTypes {
-    Section,
-    ResellerSection,
-}
 
 export class Overview implements OrganismInterface {
     title: string;
     subtitle?: string;
-    sections: SectionTypes[];
+    sections: Section[];
     ctas?: CtaBlock;
 }
