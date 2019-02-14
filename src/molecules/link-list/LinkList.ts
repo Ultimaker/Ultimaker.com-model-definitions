5b4c2198c4276e77bd81f89caa7c4f9d187f605f
Alexandros Chalvantzis <chalvantzis12@outlook.com>
2019-02-14 15:04:12 +0100

    add Link model back

--- src/molecules/link-list/LinkList.ts
+++ src/molecules/link-list/LinkList.ts
@@ -1,8 +1,9 @@
+import { Link } from 'atoms/cta/Link';
 import { ListHeading } from 'atoms/ListHeading/ListHeading';
 
 export class LinkList {
     lists?: LinkList[];
-    links?: ListHeading[];
-    link: ListHeading;
-    bottomLink?: ListHeading;
+    links?: Link[];
+    listHeading: ListHeading;
+    bottomLink?: Link;
 }
