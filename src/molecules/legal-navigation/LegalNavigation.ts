ba7c25371cdb3796b3cee6608dd6ade9e7680a02
zwaardje <niels@seiton.io>
2019-02-19 13:40:30 +0100

    chore(comments) pr comments
    Add legal navigation

---
+++ src/molecules/legal-navigation/LegalNavigation.ts
@@ -0,0 +1,6 @@
+import { NavigationItem } from '../navigation-item/NavigationItem';
+
+export class LegalNavigation {
+    navigation: NavigationItem[];
+    label: string;
+}
--- src/organisms/page-footer/PageFooter.ts
+++ src/organisms/page-footer/PageFooter.ts
@@ -1,8 +1,9 @@
 import { NavigationItem } from '../../molecules/navigation-item/NavigationItem';
 import { CountryAutoCompleteField } from '../../molecules/fields/CountryAutoCompleteField';
+import { LegalNavigation } from '../../molecules/legal-navigation/LegalNavigation';
 
 export class PageFooter {
     navigation: NavigationItem[];
     localeSelector: CountryAutoCompleteField;
-    legalNavigation: NavigationItem[];
+    legalNavigation: LegalNavigation;
 }
