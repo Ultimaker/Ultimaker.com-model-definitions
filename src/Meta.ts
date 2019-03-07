83c667486d4d702bd99a6cafb58f0d45a13e144e
Etiënne van den Tempel <e.vandentempel@ultimaker.com>
2019-03-07 15:19:14 +0100

    Add Meta object to Page

---
+++ src/Meta.ts
@@ -0,0 +1,7 @@
+import { Image } from './atoms/media/Image';
+
+export class Meta {
+    title: string;
+    description: string;
+    image: Image;
+}
--- src/atoms/media/Image.ts
+++ src/atoms/media/Image.ts
@@ -1,4 +1,5 @@
 export class Image {
     alt: string;
     url: string;
+    mimeType: string;
 }
