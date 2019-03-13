06a3cfe2c0390b10f036a528054f8846158854d8
Robin van Dijk <r.vandijk@developers.nl>
2019-03-13 13:57:58 +0100

    Change showall to expand

--- src/molecules/sections/ListSection.ts
+++ src/molecules/sections/ListSection.ts
@@ -1,13 +1,12 @@
 import { Tooltip } from '../../atoms/tooltip/Tooltip';
 import { Limit } from '../../atoms/limit/Limit';
 import { Card } from '../cards/Card';
-import { AnalyticalEvent } from '../../events/AnalyticalEvent';
+import { Expand } from './Expand';
 
 export class ListSection {
     cards: Card[];
+    expand?: Expand;
+    limit?: Limit;
     title?: string;
     tooltip?: Tooltip;
-    limit?: Limit;
-    showAllLabel?: string;
-    clickEvent?: AnalyticalEvent;
 }
