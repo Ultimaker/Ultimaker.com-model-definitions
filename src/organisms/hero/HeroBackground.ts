d15b2ed2e669133c7dcd42873b79452da7a24340
Etiënne van den Tempel <e.vandentempel@ultimaker.com>
2019-03-25 13:22:45 +0100

    Introduce HeroBackground

---
+++ src/organisms/hero/HeroBackground.ts
@@ -0,0 +1,8 @@
+import { Hero } from './Hero';
+import { Image } from '../../atoms/media/Image';
+
+export class HeroBackground extends Hero {
+    image_small?: Image;
+    image_medium?: Image;
+    image_large?: Image;
+}
