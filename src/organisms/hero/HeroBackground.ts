09e6dd4c1e7f730f43c78c581041fdc472e979aa
Etiënne van den Tempel <e.vandentempel@ultimaker.com>
2019-03-25 13:28:17 +0100

    camelCase to make TSLint happy

--- src/organisms/hero/HeroBackground.ts
+++ src/organisms/hero/HeroBackground.ts
@@ -2,7 +2,7 @@ import { Hero } from './Hero';
 import { Image } from '../../atoms/media/Image';
 
 export class HeroBackground extends Hero {
-    image_small?: Image;
-    image_medium?: Image;
-    image_large?: Image;
+    imageSmall?: Image;
+    imageMedium?: Image;
+    imageLarge?: Image;
 }
