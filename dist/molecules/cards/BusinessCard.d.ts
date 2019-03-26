b17c52519ffc8917cb146a7c905baa5d6d79f268
Robin van Dijk <r.vandijk@developers.nl>
2019-03-26 15:15:27 +0100

    Move contentLink to businesscard, add url property to cardProduct

--- dist/molecules/cards/BusinessCard.d.ts
+++ dist/molecules/cards/BusinessCard.d.ts
@@ -1,5 +1,7 @@
 import { Card } from './Card';
 import { Property } from '../../atoms/properties/Property';
+import { ContentLink } from '../../atoms/cta/ContentLink';
 export declare class BusinessCard extends Card {
+    contentLink: ContentLink;
     properties?: Property[];
 }
