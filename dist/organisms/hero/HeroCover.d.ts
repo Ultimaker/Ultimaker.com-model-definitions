cff45477dd644f45f3ce5c4c0d60635be4d8d5aa
Robbert Helling <robbert@codebron.nl>
2019-03-26 13:45:40 +0100

    refactor: HeroBackground -> HeroCover

--- dist/organisms/hero/HeroCover.d.ts
+++ dist/organisms/hero/HeroCover.d.ts
@@ -1,7 +1,6 @@
 import { Hero } from './Hero';
 import { Image } from '../../atoms/media/Image';
-
-export class HeroBackground extends Hero {
+export declare class HeroCover extends Hero {
     imageSmall?: Image;
     imageMedium?: Image;
     imageLarge?: Image;
