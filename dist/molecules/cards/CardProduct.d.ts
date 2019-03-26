8c9a7500dd7c162076e73618985f430a273b015c
Robin van Dijk <r.vandijk@developers.nl>
2019-03-26 14:48:04 +0100

    Add Card Product

---
+++ dist/molecules/cards/CardProduct.d.ts
@@ -0,0 +1,4 @@
+import { Card } from './Card';
+export declare class CardProduct extends Card {
+    properties?: string[];
+}
---
+++ dist/molecules/cards/CardProduct.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const Card_1 = require("./Card");
+class CardProduct extends Card_1.Card {
+}
+exports.CardProduct = CardProduct;
+//# sourceMappingURL=CardProduct.js.map
