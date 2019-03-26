2a71dc477ef59e49c44e4f62f87e47ca4e03d884
Robin van Dijk <r.vandijk@developers.nl>
2019-03-26 15:17:37 +0100

    Up version to 1.0.0

--- src/molecules/cards/BusinessCard.ts
+++ src/molecules/cards/BusinessCard.ts
@@ -1,6 +1,6 @@
 import { Card } from './Card';
 import { Property } from '../../atoms/properties/Property';
-import {ContentLink} from '../../atoms/cta/ContentLink';
+import { ContentLink } from '../../atoms/cta/ContentLink';
 
 export class BusinessCard extends Card {
     contentLink: ContentLink;
