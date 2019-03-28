19f675896a738d32d0e8c8b1440853e4f154cbca
Robin van Dijk <r.vandijk@developers.nl>
2019-03-28 15:21:38 +0100

    add optional focusArea

--- dist/atoms/media/Image.js
+++ dist/atoms/media/Image.js
@@ -1,5 +1,19 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+var FocusArea;
+(function (FocusArea) {
+    FocusArea["center"] = "";
+    FocusArea["top"] = "top";
+    FocusArea["right"] = "right";
+    FocusArea["bottom"] = "bottom";
+    FocusArea["left"] = "left";
+    FocusArea["topRight"] = "top_right";
+    FocusArea["topLeft"] = "top_left";
+    FocusArea["bottomRight"] = "bottom_right";
+    FocusArea["bottomLeft"] = "bottom_left";
+    FocusArea["faceLargest"] = "face";
+    FocusArea["faces"] = "faces";
+})(FocusArea = exports.FocusArea || (exports.FocusArea = {}));
 class Image {
 }
 exports.Image = Image;
--- src/atoms/media/Image.ts
+++ src/atoms/media/Image.ts
@@ -1,5 +1,20 @@
+export enum FocusArea {
+    center = '', // default, no need to specify
+    top = 'top',
+    right = 'right',
+    bottom = 'bottom',
+    left = 'left',
+    topRight = 'top_right',
+    topLeft = 'top_left',
+    bottomRight = 'bottom_right',
+    bottomLeft = 'bottom_left',
+    faceLargest = 'face',
+    faces = 'faces',
+}
+
 export class Image {
     alt: string;
     url: string;
     mimeType: string;
+    focusArea?: FocusArea;
 }
