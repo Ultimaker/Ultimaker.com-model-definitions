9d4e6ad7ae591b955d02e2d0092a703bdcb8216e
zwaardje <niels@seiton.io>
2019-03-28 09:16:48 +0100

    chore(dist): add dist files

---
+++ dist/molecules/contentGeneral/ContentGeneral.d.ts
@@ -0,0 +1,9 @@
+import { Image } from '../../atoms/media/Image';
+import { CtaBlock } from '../cta/CtaBlock';
+export declare class ContentGeneral {
+    title: string;
+    description: string;
+    image: Image;
+    ctas: CtaBlock;
+    reversed?: boolean;
+}
---
+++ dist/molecules/tab/Tab.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class Tab {
+}
+exports.Tab = Tab;
+//# sourceMappingURL=Tab.js.map
