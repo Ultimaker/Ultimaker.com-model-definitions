0ceb95ed093aaf3c935e03b3f979f81fc501ccfd
Robin van Dijk <r.vandijk@developers.nl>
2019-03-28 15:21:38 +0100

    add optional focusArea

--- dist/atoms/media/Image.d.ts
+++ dist/atoms/media/Image.d.ts
@@ -1,5 +1,19 @@
+export declare enum FocusArea {
+    center = "",
+    top = "top",
+    right = "right",
+    bottom = "bottom",
+    left = "left",
+    topRight = "top_right",
+    topLeft = "top_left",
+    bottomRight = "bottom_right",
+    bottomLeft = "bottom_left",
+    faceLargest = "face",
+    faces = "faces"
+}
 export declare class Image {
     alt: string;
     url: string;
     mimeType: string;
+    focusArea?: FocusArea;
 }
--- dist/organisms/hero/HeroCover.d.ts
+++ dist/organisms/hero/HeroCover.d.ts
@@ -1,6 +1,5 @@
 import { Hero } from './Hero';
 import { Image } from '../../atoms/media/Image';
-
 export declare class HeroCover extends Hero {
     image?: Image;
 }
