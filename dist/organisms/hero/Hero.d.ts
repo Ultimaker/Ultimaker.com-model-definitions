7c91809dd62d8938ee8a8d20ee8f94d4ea348d60
Michiel van Alphen <m.vanalphen@ultimaker.com>
2019-04-03 13:38:43 +0200

    build

--- dist/organisms/hero/Hero.d.ts
+++ dist/organisms/hero/Hero.d.ts
@@ -2,7 +2,6 @@ import { CtaBlock } from '../../molecules/cta/CtaBlock';
 import { OrganismInterface } from '../OrganismInterface';
 export declare abstract class Hero implements OrganismInterface {
     title: string;
-    subtitle?: string;
     description: string;
     ctas?: CtaBlock;
 }
--- dist/organisms/hero/HeroBasic.d.ts
+++ dist/organisms/hero/HeroBasic.d.ts
@@ -1,5 +1,6 @@
 import { Hero } from './Hero';
 import { Image } from '../../atoms/media/Image';
 export declare class HeroBasic extends Hero {
+    subtitle?: string;
     image?: Image;
 }
