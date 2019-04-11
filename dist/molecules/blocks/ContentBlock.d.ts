ffe3760d167b4514d207431fb14a567a7d563abf
zwaardje <niels@seiton.io>
2019-04-11 09:13:20 +0200

    feat(contentBlocks): add contentBlocks
    Add contentBlocks
    Add new build
    Update list section

---
+++ dist/molecules/blocks/ContentBlock.d.ts
@@ -0,0 +1,9 @@
+import { Image } from '../../atoms/media/Image';
+import { ContentButton } from '../../atoms/cta/ContentButton';
+import { ContentLink } from '../../atoms/cta/ContentLink';
+export declare class ContentBlock {
+    title: string;
+    description: string;
+    image?: Image;
+    cta?: ContentButton | ContentLink;
+}
--- dist/molecules/sections/ListSection.d.ts
+++ dist/molecules/sections/ListSection.d.ts
@@ -1,9 +1,10 @@
 import { Tooltip } from '../../atoms/tooltip/Tooltip';
 import { Limit } from '../../atoms/limit/Limit';
-import { Card } from '../cards/Card';
+import { BusinessCard } from '../cards/BusinessCard';
+import { ContentBlock } from '../blocks/ContentBlock';
 import { ContentButton } from '../../atoms/cta/ContentButton';
 export declare class ListSection {
-    cards: Card[];
+    cards: BusinessCard[] | ContentBlock[];
     expand?: ContentButton;
     limit?: Limit;
     title?: string;
