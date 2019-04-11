2d95212e92d5d6ad275c66f9ca6f79be65476962
Robin van Dijk <r.vandijk@developers.nl>
2019-04-11 14:44:31 +0200

    Only allow contentlink and contentbutton

--- src/molecules/contentGeneral/ContentGeneral.ts
+++ src/molecules/contentGeneral/ContentGeneral.ts
@@ -1,10 +1,11 @@
 import { Image } from '../../atoms/media/Image';
-import { CtaBlock } from '../cta/CtaBlock';
+import { ContentLink } from '../../atoms/cta/ContentLink';
+import { ContentButton } from '../../atoms/cta/ContentButton';
 
 export class ContentGeneral {
     title: string;
     description: string;
     image?: Image;
-    ctas?: CtaBlock;
+    cta?: ContentLink | ContentButton;
     reversed: boolean;
 }
