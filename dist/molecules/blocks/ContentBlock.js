aaa26a8ee0ace4d26e0ae1bc3131718657fc6620
Robin van Dijk <r.vandijk@developers.nl>
2019-04-11 13:32:59 +0200

    Fix

--- dist/molecules/blocks/ContentBlock.js
+++ dist/molecules/blocks/ContentBlock.js
@@ -1,3 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+class ContentBlock {
+}
+exports.ContentBlock = ContentBlock;
 //# sourceMappingURL=ContentBlock.js.map
--- src/molecules/blocks/ContentBlock.ts
+++ src/molecules/blocks/ContentBlock.ts
@@ -2,7 +2,7 @@ import { Image } from '../../atoms/media/Image';
 import { ContentButton } from '../../atoms/cta/ContentButton';
 import { ContentLink } from '../../atoms/cta/ContentLink';
 
-export declare class ContentBlock {
+export class ContentBlock {
     title: string;
     description: string;
     image?: Image;
