11decd56b2a49bfad0fb839a6656effc2c0e5868
zwaardje <niels@seiton.io>
2019-04-16 15:01:49 +0200

    fix(youtubelink): Add youtubelink
    Add youtubelink cta

--- src/molecules/contentCover/ContentCover.ts
+++ src/molecules/contentCover/ContentCover.ts
@@ -1,10 +1,11 @@
 import { Image } from '../../atoms/media/Image';
 import { ContentLink } from '../../atoms/cta/ContentLink';
 import { ContentButton } from '../../atoms/cta/ContentButton';
+import { YoutubeLink } from '../../atoms/cta/YoutubeLink';
 
 export class ContentCover {
     title: string;
     description: string;
     image?: Image;
-    cta?: ContentLink | ContentButton;
+    cta?: ContentLink | ContentButton | YoutubeLink;
 }
