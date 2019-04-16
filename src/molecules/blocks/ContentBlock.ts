c6475d5817ce918eeb39973f17afe991903c4290
Robin van Dijk <r.vandijk@developers.nl>
2019-04-16 14:36:06 +0200

    Add YoutubeLink as CTA option for content block

--- src/molecules/blocks/ContentBlock.ts
+++ src/molecules/blocks/ContentBlock.ts
@@ -1,10 +1,11 @@
 import { Image } from '../../atoms/media/Image';
 import { ContentButton } from '../../atoms/cta/ContentButton';
 import { ContentLink } from '../../atoms/cta/ContentLink';
+import { YoutubeLink } from '../../atoms/cta/YoutubeLink';
 
 export class ContentBlock {
     title: string;
     description: string;
     image?: Image;
-    cta?: ContentButton | ContentLink;
+    cta?: ContentButton | ContentLink | YoutubeLink;
 }
