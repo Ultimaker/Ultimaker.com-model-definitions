f58e92a25131d4146c36a8ba6cddc4875a3f925d
Robin van Dijk <r.vandijk@developers.nl>
2019-04-17 12:50:59 +0200

    Add CardProduct

--- dist/molecules/sections/ListSection.d.ts
+++ dist/molecules/sections/ListSection.d.ts
@@ -3,8 +3,9 @@ import { Limit } from '../../atoms/limit/Limit';
 import { BusinessCard } from '../cards/BusinessCard';
 import { ContentBlock } from '../blocks/ContentBlock';
 import { ContentButton } from '../../atoms/cta/ContentButton';
+import { CardProduct } from '../cards/CardProduct';
 export declare class ListSection {
-    cards: BusinessCard[] | ContentBlock[];
+    cards: BusinessCard[] | ContentBlock[] | CardProduct[];
     expand?: ContentButton;
     limit?: Limit;
     title?: string;
