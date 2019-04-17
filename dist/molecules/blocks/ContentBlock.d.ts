5dce3eb582ea922d25579dac764e12180644e81e
Robin van Dijk <r.vandijk@developers.nl>
2019-04-17 11:23:41 +0200

    Up version

--- dist/molecules/blocks/ContentBlock.d.ts
+++ dist/molecules/blocks/ContentBlock.d.ts
@@ -1,9 +1,10 @@
 import { Image } from '../../atoms/media/Image';
 import { ContentButton } from '../../atoms/cta/ContentButton';
 import { ContentLink } from '../../atoms/cta/ContentLink';
+import { YoutubeLink } from '../../atoms/cta/YoutubeLink';
 export declare class ContentBlock {
     title: string;
     description: string;
     image?: Image;
-    cta?: ContentButton | ContentLink;
+    cta?: ContentButton | ContentLink | YoutubeLink;
 }
