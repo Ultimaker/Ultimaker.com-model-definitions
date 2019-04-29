b41cbb95833718fdfd7bf2d07cc5fe5e44465f18
Robin van Dijk <r.vandijk@developers.nl>
2019-04-29 11:52:46 +0200

    Remove abbreviation, highlighted and properties

--- src/molecules/products/MaterialProductProperties.ts
+++
@@ -1,5 +0,0 @@
-import { ProductProperties } from './ProductProperties';
-
-export class MaterialProductProperties implements ProductProperties {
-    weight: string;
-}
--- src/molecules/products/ProductProperties.ts
+++
@@ -1,3 +0,0 @@
-export interface ProductProperties {
-    [key: string]: any;
-}
