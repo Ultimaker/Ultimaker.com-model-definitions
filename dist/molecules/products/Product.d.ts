5797197a4ed28c292401636e5968f706e87d6df5
Robin van Dijk <r.vandijk@developers.nl>
2019-04-29 11:30:04 +0200

    Add highlighted property

--- dist/molecules/products/Product.d.ts
+++ dist/molecules/products/Product.d.ts
@@ -3,6 +3,7 @@ import { ProductProperties } from './ProductProperties';
 export declare class Product {
     abbreviation: string;
     icon: string;
+    highlighted: string;
     name: string;
     pricing: ProductPricing;
     properties: ProductProperties;
--- src/molecules/products/Product.ts
+++ src/molecules/products/Product.ts
@@ -4,6 +4,7 @@ import { ProductProperties } from './ProductProperties';
 export class Product {
     abbreviation: string;
     icon: string;
+    highlighted: string;
     name: string;
     pricing: ProductPricing;
     properties: ProductProperties;
