0f9a485eb0df6ec04a3caa9ba962cc116592c274
Robbert Helling <robbert@codebron.nl>
2019-05-08 13:53:30 +0200

    feat(colors): model definition

---
+++ dist/molecules/products/ProductColor.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class ProductColor {
+}
+exports.ProductColor = ProductColor;
+//# sourceMappingURL=ProductColor.js.map
