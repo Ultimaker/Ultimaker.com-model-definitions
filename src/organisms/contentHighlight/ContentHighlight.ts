9ce0f861eebf0d41ba6feb9e2eaed0b44464da80
zwaardje <niels@seiton.io>
2019-05-20 09:36:57 +0200

    feat(contentHighlight): Add content highlight

---
+++ src/organisms/contentHighlight/ContentHighlight.ts
@@ -0,0 +1,12 @@
+import { OrganismInterface } from '../OrganismInterface';
+import { CtaBlock } from '../../molecules/cta/CtaBlock';
+import { Image } from '../../atoms/media/Image';
+
+export class ContentHighlight implements OrganismInterface{
+    title: string;
+    subtitle?: string;
+    description: string;
+    image?: Image;
+    ctas?: CtaBlock;
+    reversed: boolean;
+}
