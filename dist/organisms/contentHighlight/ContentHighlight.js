a9a114fd6913c29802edda933308f2a933bc8280
zwaardje <niels@seiton.io>
2019-05-20 09:36:57 +0200

    feat(contentHighlight): Add content highlight

---
+++ dist/organisms/contentHighlight/ContentHighlight.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class ContentHighlight {
+}
+exports.ContentHighlight = ContentHighlight;
+//# sourceMappingURL=ContentHighlight.js.map
