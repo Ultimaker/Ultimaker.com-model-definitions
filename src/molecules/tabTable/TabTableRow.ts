e7492c05cc07acb8803592810c0372f3f692edf3
Michiel van Alphen <m.vanalphen@ultimaker.com>
2019-05-23 16:57:50 +0200

    added tooltip interface

--- src/molecules/tabTable/TabTableRow.ts
+++ src/molecules/tabTable/TabTableRow.ts
@@ -1,7 +1,8 @@
 import { Tooltip } from '../../atoms/tooltip/Tooltip';
 import { ContentLink } from '../../atoms/cta/ContentLink';
+import { Tooltippable } from '../../atoms/tooltip/Tooltippable';
 
-export class TabTableRow  {
+export class TabTableRow implements Tooltippable {
     label: string;
     tooltip?: Tooltip;
     value?: string;
