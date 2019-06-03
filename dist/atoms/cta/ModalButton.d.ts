396ab1aac3be62f36d429f1eb47d5136b0051be3
Michiel van Alphen <m.vanalphen@ultimaker.com>
2019-06-03 14:15:41 +0200

    build

---
+++ dist/atoms/cta/ModalButton.d.ts
@@ -0,0 +1,5 @@
+import { Cta } from './Cta';
+import { ModalWizard } from '../../organisms/modal/ModalWizard';
+export declare class ModalButton extends Cta {
+    modal: ModalWizard;
+}
---
+++ dist/atoms/cta/SubmitButton.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const Cta_1 = require("./Cta");
+class SubmitButton extends Cta_1.Cta {
+}
+exports.SubmitButton = SubmitButton;
+//# sourceMappingURL=SubmitButton.js.map
