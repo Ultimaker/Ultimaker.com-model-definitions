6aa4223a86538cdcf894b2843f93ba4945c2ed07
Michiel van Alphen <m.vanalphen@ultimaker.com>
2019-06-03 14:14:27 +0200

    added models

---
+++ src/organisms/modal/ModalWizard.ts
@@ -0,0 +1,8 @@
+import { OrganismInterface } from '../OrganismInterface';
+import { WizardStepInterface } from './WizardStepInterface';
+
+export class ModalWizard implements OrganismInterface {
+    title: string;
+    steps: WizardStepInterface[];
+
+}
