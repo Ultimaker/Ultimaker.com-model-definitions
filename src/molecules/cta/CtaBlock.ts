e41f7a4db27003a036ff48ed80c9b65565782acc
Michiel van Alphen <m.vanalphen@ultimaker.com>
2019-06-03 14:56:46 +0200

    added button types to cta block

--- src/molecules/cta/CtaBlock.ts
+++ src/molecules/cta/CtaBlock.ts
@@ -1,6 +1,8 @@
 import { ContentButton } from '../../atoms/cta/ContentButton';
 import { ContentLink } from '../../atoms/cta/ContentLink';
+import { YoutubeLink } from '../../atoms/cta/YoutubeLink';
+import { ModalButton } from '../../atoms/cta/ModalButton';
 
 export class CtaBlock {
-    ctas: (ContentButton | ContentLink)[];
+    ctas: (ContentButton | ContentLink | YoutubeLink | ModalButton)[];
 }
