2fd0b153303a696500538d61cd15d8a7f3bad975
Michiel van Alphen <m.vanalphen@ultimaker.com>
2019-06-03 14:15:41 +0200

    build

---
+++ dist/atoms/cta/ModalButton.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const Cta_1 = require("./Cta");
+class ModalButton extends Cta_1.Cta {
+}
+exports.ModalButton = ModalButton;
+//# sourceMappingURL=ModalButton.js.map
