9da0b3a453efd5daf114f2c25a01aa79d048b075
Michiel van Alphen <m.vanalphen@ultimaker.com>
2019-06-04 14:49:21 +0200

    tsc

---
+++ dist/molecules/wizard/ModalButton.d.ts
@@ -0,0 +1,8 @@
+import { ModalWizard } from './ModalWizard';
+import { AnalyticalEvent } from '../../events/AnalyticalEvent';
+export declare class ModalButton {
+    clickEvent?: AnalyticalEvent;
+    icon?: string;
+    label: string;
+    modal: ModalWizard;
+}
