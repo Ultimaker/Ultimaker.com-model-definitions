997c152d71c8385a292ad8c17aa007226923e98c
Michiel van Alphen <m.vanalphen@ultimaker.com>
2019-06-04 09:39:04 +0200

    updated dist

--- dist/molecules/cta/CtaBlock.d.ts
+++ dist/molecules/cta/CtaBlock.d.ts
@@ -1,7 +1,11 @@
 import { ContentButton } from '../../atoms/cta/ContentButton';
 import { ContentLink } from '../../atoms/cta/ContentLink';
 import { YoutubeLink } from '../../atoms/cta/YoutubeLink';
+<<<<<<< HEAD
 import { ModalButton } from '../wizard/ModalButton';
+=======
+import { ModalButton } from '../../atoms/cta/ModalButton';
+>>>>>>> updated dist
 export declare class CtaBlock {
     ctas: (ContentButton | ContentLink | YoutubeLink | ModalButton)[];
 }
