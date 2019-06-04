b03d880f3f13c9d75dc0950b852b87182ae2b510
Michiel van Alphen <m.vanalphen@ultimaker.com>
2019-06-04 14:23:09 +0200

    moved wizard modal to molecules

--- dist/molecules/wizardStep/WizardStepDownloadList.d.ts
+++
@@ -1,9 +0,0 @@
-import { WizardStepInterface } from '../../organisms/modal/WizardStepInterface';
-import { SubmitButton } from '../../atoms/cta/SubmitButton';
-import { CardDownload } from '../cards/CardDownload';
-export declare class WizardStepDownloadList implements WizardStepInterface {
-    title: string;
-    subtitle?: string;
-    items: CardDownload[];
-    cta: SubmitButton;
-}
--- src/molecules/wizard/ModalWizard.ts
+++ src/molecules/wizard/ModalWizard.ts
@@ -1,6 +1,7 @@
+import { OrganismInterface } from '../../organisms/OrganismInterface';
 import { WizardStepInterface } from './WizardStepInterface';
 
-export class ModalWizard {
+export class ModalWizard implements OrganismInterface {
     title: string;
     steps: WizardStepInterface[];
 }
