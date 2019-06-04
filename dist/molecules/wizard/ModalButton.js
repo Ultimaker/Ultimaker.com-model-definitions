9b1ef29915012f651cfbe4887bb82cafec897bc8
Michiel van Alphen <m.vanalphen@ultimaker.com>
2019-06-04 14:49:21 +0200

    tsc

--- dist/molecules/wizard/ModalButton.js
+++ dist/molecules/wizard/ModalButton.js
@@ -1,7 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const Cta_1 = require("./Cta");
-class ModalButton extends Cta_1.Cta {
+class ModalButton {
 }
 exports.ModalButton = ModalButton;
 //# sourceMappingURL=ModalButton.js.map
