5d9b39ede9b70c80d0ec11ce55bf962242963ca3
zwaardje <niels@seiton.io>
2019-06-06 11:54:26 +0200

    chore(build): build

--- dist/molecules/cta/CtaBlock.d.ts
+++ dist/molecules/cta/CtaBlock.d.ts
@@ -1,15 +1,7 @@
 import { ContentButton } from '../../atoms/cta/ContentButton';
 import { ContentLink } from '../../atoms/cta/ContentLink';
 import { YoutubeLink } from '../../atoms/cta/YoutubeLink';
-<<<<<<< HEAD
-<<<<<<< HEAD
 import { ModalButton } from '../wizard/ModalButton';
-=======
-import { ModalButton } from '../../atoms/cta/ModalButton';
->>>>>>> updated dist
-=======
-import { ModalButton } from '../wizard/ModalButton';
->>>>>>> tsc
 export declare class CtaBlock {
     ctas: (ContentButton | ContentLink | YoutubeLink | ModalButton)[];
 }
