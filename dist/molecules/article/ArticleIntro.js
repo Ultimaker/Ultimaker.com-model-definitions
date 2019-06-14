701a48a88767f39342ec2c5dfa8774c3e7783507
Robbert Helling <robbert@codebron.nl>
2019-06-14 12:01:15 +0200

    feat(article-content): add ArticleContent and ArticleIntro definitions

---
+++ dist/molecules/article/ArticleIntro.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class ArticleIntro {
+}
+exports.ArticleIntro = ArticleIntro;
+//# sourceMappingURL=ArticleIntro.js.map
