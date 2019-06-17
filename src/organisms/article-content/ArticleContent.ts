58955fdcfb0837f149a54d34eafde67087709596
zwaardje <niels@seiton.io>
2019-06-17 15:15:53 +0200

    feat(article-image): add article-image

--- src/organisms/article-content/ArticleContent.ts
+++ src/organisms/article-content/ArticleContent.ts
@@ -1,7 +1,8 @@
 import { OrganismInterface } from '../OrganismInterface';
 import { ArticleIntro } from '../../molecules/article/ArticleIntro';
 import { ArticleText } from '../../molecules/article/ArticleText';
+import { ArticleImage } from '../../molecules/article/ArticleImage';
 
 export class ArticleContent implements OrganismInterface {
-    molecules: [ArticleIntro|ArticleText];
+    molecules: [ArticleIntro|ArticleText|ArticleImage];
 }
