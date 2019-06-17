fa27d8b691e164962e1f6c6c267ac94d9601b779
zwaardje <niels@seiton.io>
2019-06-17 15:15:53 +0200

    feat(article-image): add article-image

--- dist/organisms/article-content/ArticleContent.d.ts
+++ dist/organisms/article-content/ArticleContent.d.ts
@@ -1,6 +1,7 @@
 import { OrganismInterface } from '../OrganismInterface';
 import { ArticleIntro } from '../../molecules/article/ArticleIntro';
 import { ArticleText } from '../../molecules/article/ArticleText';
+import { ArticleImage } from '../../molecules/article/ArticleImage';
 export declare class ArticleContent implements OrganismInterface {
-    molecules: [ArticleIntro | ArticleText];
+    molecules: [ArticleIntro | ArticleText | ArticleImage];
 }
---
+++ src/molecules/article/ArticleImage.ts
@@ -0,0 +1,6 @@
+import { Image } from '../../atoms/media/Image';
+
+export class ArticleImage {
+    image?: Image;
+    caption?: string;
+}
