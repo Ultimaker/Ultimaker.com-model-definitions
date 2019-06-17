b87a92b17e00c4635d000c18a0806cb2865d8154
zwaardje <niels@seiton.io>
2019-06-17 11:33:12 +0200

    chore(comments): fix pr comments

--- dist/organisms/article-content/ArticleContent.d.ts
+++ dist/organisms/article-content/ArticleContent.d.ts
@@ -1,6 +1,6 @@
 import { OrganismInterface } from '../OrganismInterface';
 import { ArticleIntro } from '../../molecules/article/ArticleIntro';
-import { ArticleText } from '../../molecules/article/AritcleText';
+import { ArticleText } from '../../molecules/article/ArticleText';
 export declare class ArticleContent implements OrganismInterface {
     molecules: [ArticleIntro | ArticleText];
 }
