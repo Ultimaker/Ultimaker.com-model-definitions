ace7b252b44df37cf2e45d8a1c73c867347d5a03
Etiënne van den Tempel <e.vandentempel@ultimaker.com>
2019-06-18 15:49:59 +0200

    Add ArticleCtas

---
+++ dist/molecules/article/ArticleCtas.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class ArticleCtas {
+}
+exports.ArticleCtas = ArticleCtas;
+//# sourceMappingURL=ArticleCtas.js.map
--- src/organisms/article-content/ArticleContent.ts
+++ src/organisms/article-content/ArticleContent.ts
@@ -2,7 +2,8 @@ import { OrganismInterface } from '../OrganismInterface';
 import { ArticleIntro } from '../../molecules/article/ArticleIntro';
 import { ArticleText } from '../../molecules/article/ArticleText';
 import { ArticleImage } from '../../molecules/article/ArticleImage';
+import { ArticleCtas } from '../../molecules/article/ArticleCtas';
 
 export class ArticleContent implements OrganismInterface {
-    molecules: [ArticleIntro|ArticleText|ArticleImage];
+    molecules: [ArticleIntro|ArticleText|ArticleImage|ArticleCtas];
 }
