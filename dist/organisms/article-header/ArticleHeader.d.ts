22c8e053a6ae78024484634f4433c82b92da1a0b
zwaardje <niels@seiton.io>
2019-06-19 13:27:04 +0200

    fix(article-header): fix article header
    Move from molecule to organism

---
+++ dist/organisms/article-header/ArticleHeader.d.ts
@@ -0,0 +1,6 @@
+import { OrganismInterface } from '../OrganismInterface';
+import { ArticleAuthor } from '../../molecules/article/ArticleAuthor';
+export declare class ArticleHeader implements OrganismInterface {
+    author: ArticleAuthor;
+    publishedDate: Date;
+}
--- src/molecules/article/ArticleHeader.ts
+++
@@ -1,6 +0,0 @@
-import { ArticleAuthor } from './ArticleAuthor';
-
-export class ArticleHeader {
-    author: ArticleAuthor;
-    publishedDate: Date;
-}
