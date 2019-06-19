1bdecc38ffa1312afaf96c88c6c17ab4914fa439
zwaardje <niels@seiton.io>
2019-06-19 08:52:59 +0200

    feat(author): add article author
    Add article author
    Add article header

---
+++ dist/molecules/article/ArticleHeader.d.ts
@@ -0,0 +1,5 @@
+import { ArticleAuthor } from './ArticleAuthor';
+export declare class ArticleHeader {
+    author: ArticleAuthor;
+    publishedDate: Date;
+}
--- dist/organisms/article-content/ArticleContent.d.ts
+++ dist/organisms/article-content/ArticleContent.d.ts
@@ -3,6 +3,7 @@ import { ArticleIntro } from '../../molecules/article/ArticleIntro';
 import { ArticleText } from '../../molecules/article/ArticleText';
 import { ArticleImage } from '../../molecules/article/ArticleImage';
 import { ArticleCtas } from '../../molecules/article/ArticleCtas';
+import { ArticleVideo } from '../../molecules/article/ArticleVideo';
 export declare class ArticleContent implements OrganismInterface {
-    molecules: [ArticleIntro | ArticleText | ArticleImage | ArticleCtas];
+    molecules: [ArticleIntro | ArticleText | ArticleImage | ArticleCtas | ArticleVideo];
 }
