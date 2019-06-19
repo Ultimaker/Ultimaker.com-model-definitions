ec60e7601a0cd37d1c62dc464a7e5475f04c4e84
Robbert Helling <robbert@codebron.nl>
2019-06-19 08:12:20 +0200

    feat(article): ArticleVideo definition

---
+++ dist/molecules/article/ArticleVideo.d.ts
@@ -0,0 +1,6 @@
+import { Image } from '../../atoms/media/Image';
+import { YoutubeLink } from '../../atoms/cta/YoutubeLink';
+export declare class ArticleVideo {
+    overlay?: Image;
+    cta: YoutubeLink;
+}
