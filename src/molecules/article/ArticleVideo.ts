3be30a45de8d48018dda807a20dd790808b2b72e
Robbert Helling <robbert@codebron.nl>
2019-06-19 08:12:20 +0200

    feat(article): ArticleVideo definition

---
+++ src/molecules/article/ArticleVideo.ts
@@ -0,0 +1,7 @@
+import { Image } from '../../atoms/media/Image';
+import { YoutubeLink } from '../../atoms/cta/YoutubeLink';
+
+export class ArticleVideo {
+    overlay?: Image;
+    cta: YoutubeLink;
+}
--- src/organisms/article-content/ArticleContent.ts
+++ src/organisms/article-content/ArticleContent.ts
@@ -3,7 +3,8 @@ import { ArticleIntro } from '../../molecules/article/ArticleIntro';
 import { ArticleText } from '../../molecules/article/ArticleText';
 import { ArticleImage } from '../../molecules/article/ArticleImage';
 import { ArticleCtas } from '../../molecules/article/ArticleCtas';
+import { ArticleVideo } from '../../molecules/article/ArticleVideo';
 
 export class ArticleContent implements OrganismInterface {
-    molecules: [ArticleIntro|ArticleText|ArticleImage|ArticleCtas];
+    molecules: [ArticleIntro|ArticleText|ArticleImage|ArticleCtas|ArticleVideo];
 }
