8e145cd520bfba3df8152d73a132cd25aae70ccb
Michiel van Alphen <m.vanalphen@ultimaker.com>
2019-06-24 14:12:57 +0200

    build

---
+++ dist/molecules/navigation-item/MainNavigationItem.d.ts
@@ -0,0 +1,5 @@
+import { NavigationCategory } from './NavigationCategory';
+import { NavigationItem } from './NavigationItem';
+export declare class MainNavigationItem extends NavigationItem {
+    childCategories?: NavigationCategory[];
+}
