012464b8ef6a6a52f6609223dc7905ccceb494fa
Michiel van Alphen <m.vanalphen@ultimaker.com>
2019-06-24 13:54:22 +0200

    added proposal for changes

--- src/organisms/page-footer/PageFooter.ts
+++ src/organisms/page-footer/PageFooter.ts
@@ -1,9 +1,9 @@
-import { NavigationItem } from '../../molecules/navigation-item/NavigationItem';
 import { CountryAutoCompleteField } from '../../molecules/fields/CountryAutoCompleteField';
 import { LegalNavigation } from '../../molecules/legal-navigation/LegalNavigation';
+import { NavigationCategory } from '../../molecules/navigation-item/NavigationCategory';
 
 export class PageFooter {
-    navigation: NavigationItem[];
+    navigation: NavigationCategory[];
     localeSelector: CountryAutoCompleteField;
     legalNavigation: LegalNavigation;
 }
