c87e40ec19867b7fa725594fba9c8fca73087920
Michiel van Alphen <m.vanalphen@ultimaker.com>
2019-06-25 08:57:51 +0200

    build

---
+++ dist/molecules/navigation-item/MainNavigationCategory.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const NavigationCategory_1 = require("./NavigationCategory");
+class MainNavigationCategory extends NavigationCategory_1.NavigationCategory {
+}
+exports.MainNavigationCategory = MainNavigationCategory;
+//# sourceMappingURL=MainNavigationCategory.js.map
