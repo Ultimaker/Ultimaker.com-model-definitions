391ec62d58c11cc5716e39d827266c455115d0bf
Michiel van Alphen <m.vanalphen@ultimaker.com>
2019-07-02 13:36:10 +0200

    removed legal nav category, removed footer nav category as child class

--- dist/molecules/navigation/FooterNavigationCategory.d.ts
+++
@@ -1,3 +0,0 @@
-import { NavigationCategory } from './NavigationCategory';
-export declare class FooterNavigationCategory extends NavigationCategory {
-}
--- src/molecules/navigation/LegalNavigationCategory.ts
+++
@@ -1,4 +0,0 @@
-import { NavigationCategory } from './NavigationCategory';
-
-export class LegalNavigationCategory extends NavigationCategory{
-}
