403be0b23538323d68aa152a3c6f55554fcd58f8
Michiel van Alphen <m.vanalphen@ultimaker.com>
2019-07-02 14:13:05 +0200

    copyright label added

--- dist/organisms/page-footer/PageFooter.d.ts
+++ dist/organisms/page-footer/PageFooter.d.ts
@@ -2,6 +2,7 @@ import { CountryAutoCompleteField } from '../../molecules/fields/CountryAutoComp
 import { NavigationItem } from '../../molecules/navigation/NavigationItem';
 import { NavigationCategory } from '../../molecules/navigation/NavigationCategory';
 export declare class PageFooter {
+    copyrightLabel: string;
     navigation: NavigationCategory[];
     localeSelector: CountryAutoCompleteField;
     legalNavigation: NavigationItem[];
--- src/organisms/page-footer/PageFooter.ts
+++ src/organisms/page-footer/PageFooter.ts
@@ -3,6 +3,7 @@ import { NavigationItem } from '../../molecules/navigation/NavigationItem';
 import { NavigationCategory } from '../../molecules/navigation/NavigationCategory';
 
 export class PageFooter {
+    copyrightLabel: string;
     navigation: NavigationCategory[];
     localeSelector: CountryAutoCompleteField;
     legalNavigation: NavigationItem[];
