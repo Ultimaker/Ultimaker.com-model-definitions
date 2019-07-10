c9ce38e10da54d4c104e7e14812d637c98b1b406
Etiënne van den Tempel <e.vandentempel@ultimaker.com>
2019-07-10 08:50:03 +0200

    Fix type constraint for publishedDate (1.24.1)

--- dist/organisms/article-header/ArticleHeader.d.ts
+++ dist/organisms/article-header/ArticleHeader.d.ts
@@ -3,6 +3,6 @@ import { Label } from '../../atoms/label/Label';
 import { OrganismInterface } from '../OrganismInterface';
 export declare class ArticleHeader implements OrganismInterface {
     author: ArticleAuthor;
-    publishedDate: Date;
+    publishedDate: string;
     labels?: Label[];
 }
