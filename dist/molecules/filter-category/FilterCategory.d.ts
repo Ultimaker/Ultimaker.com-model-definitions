c262a6307e683a0864454c6fbf0a76b0c9b533fb
dan entous <d.entous@ultimaker.com>
2019-07-15 17:15:46 +0200

    chore(): update build

---
+++ dist/molecules/filter-category/FilterCategory.d.ts
@@ -0,0 +1,6 @@
+import { Label } from '../../atoms/label/Label';
+export declare class FilterCategory {
+    labels: Label[];
+    showAllLabel: string;
+    title: string;
+}
---
+++ dist/molecules/filter-category/FilterCategory.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class FilterCategory {
+}
+exports.FilterCategory = FilterCategory;
+//# sourceMappingURL=FilterCategory.js.map
