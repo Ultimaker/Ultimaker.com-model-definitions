c194c2f89b0fb39b572be9797584253531daf849
Etiënne van den Tempel <e.vandentempel@ultimaker.com>
2019-07-26 14:44:01 +0200

    Add HeroHomepage (1.25.0)

---
+++ dist/organisms/hero/HeroHomepage.d.ts
@@ -0,0 +1,6 @@
+import { Hero } from './Hero';
+import { Image } from '../../atoms/media/Image';
+export declare class HeroHomepage extends Hero {
+    image?: Image;
+    description?: string;
+}
---
+++ dist/organisms/hero/HeroHomepage.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const Hero_1 = require("./Hero");
+class HeroHomepage extends Hero_1.Hero {
+}
+exports.HeroHomepage = HeroHomepage;
+//# sourceMappingURL=HeroHomepage.js.map
