68ee4af2058a25ebf73c9c50ef15d6656bb3434c
Michiel van Alphen <m.vanalphen@ultimaker.com>
2019-07-30 10:57:13 +0200

    moved description list to article molecule

--- src/organisms/page/DescriptionList.ts
+++
@@ -1,6 +0,0 @@
-import { OrganismInterface } from '../OrganismInterface';
-import { Description } from './Description';
-
-export class DescriptionList implements OrganismInterface {
-    descriptions: Description[];
-}
