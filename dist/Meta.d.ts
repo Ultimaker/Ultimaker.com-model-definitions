1916ca0690e754bda7fac44010512746eb846410
Robin van Dijk <r.vandijk@developers.nl>
2019-08-05 14:52:56 +0200

    Add canonical to meta

--- dist/Meta.d.ts
+++ dist/Meta.d.ts
@@ -1,8 +1,10 @@
 import { Image } from './atoms/media/Image';
 import { Hreflang } from './Hreflang';
+import { Canonical } from './Canonical';
 export declare class Meta {
     title: string;
     description: string;
+    canonical: Canonical;
     hreflang?: Hreflang[];
     image?: Image;
 }
---
+++ src/Canonical.ts
@@ -0,0 +1,4 @@
+export class Canonical {
+    rel: 'canonical';
+    href: string;
+}
