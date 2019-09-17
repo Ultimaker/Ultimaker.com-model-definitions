71e7d67c28e15234e4ed64f41187fa853df784aa
Robin van Dijk <r.vandijk@developers.nl>
2019-09-17 09:29:33 +0200

    Rebase

--- dist/Page.d.ts
+++ dist/Page.d.ts
@@ -1,9 +1,11 @@
 import { OrganismInterface } from './organisms/OrganismInterface';
 import { Meta } from './Meta';
 import { SubNavigation } from './organisms/sub-navigation/SubNavigation';
+import { Notification } from './molecules/notification/Notification';
 export declare class Page {
     meta: Meta;
-    subNavigation?: SubNavigation;
     organisms: OrganismInterface[];
     language?: string;
+    notifications?: Notification[];
+    subNavigation?: SubNavigation;
 }
