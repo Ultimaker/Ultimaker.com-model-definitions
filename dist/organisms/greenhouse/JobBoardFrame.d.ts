c85c5ebd6ff281fdc47d87e430a04db7735533db
Michiel van Alphen <m.vanalphen@ultimaker.com>
2019-09-24 12:02:37 +0200

    dist

---
+++ dist/organisms/greenhouse/JobBoardFrame.d.ts
@@ -0,0 +1,3 @@
+import { OrganismInterface } from '../OrganismInterface';
+export declare class JobBoardFrame implements OrganismInterface {
+}
---
+++ dist/organisms/greenhouse/JobBoardFrame.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class JobBoardFrame {
+}
+exports.JobBoardFrame = JobBoardFrame;
+//# sourceMappingURL=JobBoardFrame.js.map
