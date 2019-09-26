ebe7ee3fc8453a900792149b023abcc4e6846b05
Michiel van Alphen <m.vanalphen@ultimaker.com>
2019-09-26 16:05:08 +0200

    Update model def

--- dist/Page.d.ts
+++ dist/Page.d.ts
@@ -1,11 +1,13 @@
 import { OrganismInterface } from './organisms/OrganismInterface';
 import { Meta } from './Meta';
 import { SubNavigation } from './organisms/sub-navigation/SubNavigation';
+import { AnalyticalEvent } from './events/AnalyticalEvent';
 import { Notification } from './molecules/notification/Notification';
 export declare class Page {
     meta: Meta;
     organisms: OrganismInterface[];
     language?: string;
+    loadEvent?: AnalyticalEvent;
     notifications?: Notification[];
     subNavigation?: SubNavigation;
 }
