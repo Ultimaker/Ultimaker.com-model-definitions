583f2e84b73af84a6e7639a7033c8f9925e88497
Michiel van Alphen <m.vanalphen@ultimaker.com>
2019-09-26 13:53:42 +0200

    added loadevent to page

--- src/Page.ts
+++ src/Page.ts
@@ -1,11 +1,13 @@
 import { OrganismInterface } from './organisms/OrganismInterface';
 import { Meta } from './Meta';
 import { SubNavigation } from './organisms/sub-navigation/SubNavigation';
+import { AnalyticalEvent } from './events/AnalyticalEvent';
 import { Notification } from './molecules/notification/Notification';
 
 export class Page {
     meta: Meta;
     organisms: OrganismInterface[];
+    loadEvent: AnalyticalEvent;
     language?: string;
     notifications?: Notification[];
     subNavigation?: SubNavigation;
