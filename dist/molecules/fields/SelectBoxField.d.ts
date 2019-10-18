2f5796531323ec37ce99088752931ec236dbad05
Sean de Leeuw <s.deleeuw@ultimaker.com>
2019-10-18 12:55:40 +0200

    feat(SelectBox): added SelectBoxField to model definition

---
+++ dist/molecules/fields/SelectBoxField.d.ts
@@ -0,0 +1,8 @@
+export declare abstract class SelectBoxField {
+    label: string;
+    placeholder: string;
+    highlightedLabel: string;
+    datasource: {
+        [key: string]: string;
+    };
+}
---
+++ dist/molecules/fields/SelectBoxField.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class SelectBoxField {
+}
+exports.SelectBoxField = SelectBoxField;
+//# sourceMappingURL=SelectBoxField.js.map
