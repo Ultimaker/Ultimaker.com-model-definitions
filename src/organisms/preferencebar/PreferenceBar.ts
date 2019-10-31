2bf8361ef6d55b556c2095e3f5214fd00897add6
Sean de Leeuw <s.deleeuw@ultimaker.com>
2019-10-31 14:57:37 +0100

    remove(): deleted languageSelect from preference bar

--- src/organisms/preferencebar/PreferenceBar.ts
+++ src/organisms/preferencebar/PreferenceBar.ts
@@ -1,13 +1,11 @@
 import { OrganismInterface } from '../OrganismInterface';
 import { ComboBoxField } from '../../molecules/fields/ComboBoxField';
-import { SelectBoxField } from '../../molecules/fields/SelectBoxField';
 
 export class PreferenceBar implements OrganismInterface {
     title: string;
     intro: string;
     yourCountryLabel: string;
     countrySelect: ComboBoxField;
-    localeSelect: SelectBoxField;
     yourPrivacySettingsLabel: string;
     fullExperienceLabel: string;
     customExperienceLabel: string;
