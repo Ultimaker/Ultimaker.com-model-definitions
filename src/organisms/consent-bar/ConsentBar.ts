e965b9fa4ee298cb25fe59700c49e3607f7c5577
Etiënne van den Tempel <e.vandentempel@ultimaker.com>
2019-11-06 10:39:17 +0100

    Add ConsentBar

---
+++ src/organisms/consent-bar/ConsentBar.ts
@@ -0,0 +1,7 @@
+import { OrganismInterface } from '../OrganismInterface';
+
+export class ConsentBar implements OrganismInterface {
+    descriptionText: string;
+    agreeLabel: string;
+    disagreeLabel: string;
+}
