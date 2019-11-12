6221591a71efa062d540b048d597460356ac2995
dan entous <d.entous@ultimaker.com>
2019-11-12 09:49:11 +0100

    add(): OverviewResellers definition

---
+++ src/organisms/overview/OverviewResellers.ts
@@ -0,0 +1,6 @@
+import { ComboBoxField } from '../../molecules/fields/ComboBoxField';
+import { Overview } from './Overview';
+
+export class OverviewResellers extends Overview {
+    countrySelector: ComboBoxField;
+}
