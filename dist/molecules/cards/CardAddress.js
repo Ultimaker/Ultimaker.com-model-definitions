55eaaf91cef5b48690fa39009b2a4a975fafb017
Etiënne van den Tempel <e.vandentempel@ultimaker.com>
2019-12-09 13:22:41 +0100

    Add dist files

---
+++ dist/molecules/cards/CardAddress.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const Card_1 = require("./Card");
+class CardAddress extends Card_1.Card {
+}
+exports.CardAddress = CardAddress;
+//# sourceMappingURL=CardAddress.js.map
---
+++ dist/organisms/map/Marker.d.ts
@@ -0,0 +1,6 @@
+import { CardAddress } from '../../molecules/cards/CardAddress';
+export declare class Marker {
+    latitude: number;
+    longitude: number;
+    card: CardAddress;
+}
