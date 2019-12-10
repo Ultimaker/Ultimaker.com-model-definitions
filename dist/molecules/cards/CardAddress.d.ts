dd53b9a2640898382b897b766ceb2d687679bb61
Etiënne van den Tempel <e.vandentempel@ultimaker.com>
2019-12-10 14:21:00 +0100

    Remove address

--- dist/molecules/cards/CardAddress.d.ts
+++ dist/molecules/cards/CardAddress.d.ts
@@ -1,6 +1,5 @@
 import { Card } from './Card';
 import { ContentLink } from '../../atoms/cta/ContentLink';
 export declare class CardAddress extends Card {
-    address: string;
     contentLink: ContentLink;
 }
