28d89f4a4929146aeef4a06672240a5d3612c14a
Etiënne van den Tempel <e.vandentempel@ultimaker.com>
2019-12-11 11:37:17 +0100

    Add dist files

--- dist/molecules/map/Marker.d.ts
+++ dist/molecules/map/Marker.d.ts
@@ -1,4 +1,4 @@
-import { BusinessCard } from '../../../molecules/cards/BusinessCard';
+import { BusinessCard } from '../cards/BusinessCard';
 export declare class Marker {
     latitude: number;
     longitude: number;
