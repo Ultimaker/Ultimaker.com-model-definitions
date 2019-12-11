c66c023ba28131dbc28fc9f33fab73552e796ab9
Etiënne van den Tempel <e.vandentempel@ultimaker.com>
2019-12-11 11:37:17 +0100

    Add dist files

--- dist/organisms/overview/OverviewResellers.d.ts
+++ dist/organisms/overview/OverviewResellers.d.ts
@@ -1,6 +1,6 @@
 import { ComboBoxField } from '../../molecules/fields/ComboBoxField';
 import { Overview } from './Overview';
-import { ResellerMap } from './map/ResellerMap';
+import { ResellerMap } from '../../molecules/map/ResellerMap';
 export declare class OverviewResellers extends Overview {
     countrySelector: ComboBoxField;
     resellerMap?: ResellerMap;
