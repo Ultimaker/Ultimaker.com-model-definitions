07ba1d9be4700c1b753cb16160de255b0dafc838
Etiënne van den Tempel <e.vandentempel@ultimaker.com>
2020-01-20 10:51:58 +0100

    Model definitions for json-ld data in pages

--- dist/Meta.d.ts
+++ dist/Meta.d.ts
@@ -1,10 +1,12 @@
 import { Image } from './atoms/media/Image';
 import { Hreflang } from './Hreflang';
 import { Canonical } from './Canonical';
+import { Thing, WithContext } from 'schema-dts';
 export declare class Meta {
     title: string;
     description: string;
     canonical: Canonical;
     hreflang?: Hreflang[];
     image?: Image;
+    structuredData?: WithContext<Thing>[];
 }
