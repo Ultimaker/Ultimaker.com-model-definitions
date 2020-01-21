69539e29a2b6a1d7b4488b6ca17f027ea5bc41a8
dan entous <d.entous@ultimaker.com>
2020-01-21 05:13:22 +0100

    update(): dist

---
+++ dist/molecules/modal/Modal.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class Modal {
+}
+exports.Modal = Modal;
+//# sourceMappingURL=Modal.js.map
--- dist/molecules/wizard/ModalWizard.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class ModalWizard {
-}
-exports.ModalWizard = ModalWizard;
-//# sourceMappingURL=ModalWizard.js.map
