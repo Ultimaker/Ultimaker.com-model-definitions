b9b8a10e8ca4ae950727a3291b2f39e48538f50f
dan entous <d.entous@ultimaker.com>
2020-02-05 06:14:26 +0100

    add(): heading, subheading and description to FormSection

--- src/organisms/form/FormSection.ts
+++ src/organisms/form/FormSection.ts
@@ -1,6 +1,10 @@
 import { Component } from '../../Component';
 import { Form } from './Form';
+import { Heading } from '../../atoms/heading/Heading';
+import { Paragraph } from '../../atoms/paragraph/Paragraph';
 
 export class FormSection implements Component {
+    heading?: Heading;
+    description?: Paragraph;
     form: Form;
 }
