9a8e3906dc0476238405b1bc87aaa0bf003a1f01
dan entous <d.entous@ultimaker.com>
2020-02-05 05:47:36 +0100

    add(): Section model

--- src/atoms/cta/Cta.ts
+++ src/atoms/cta/Cta.ts
@@ -1,6 +1,7 @@
 import { AnalyticalEvent } from '../../events/AnalyticalEvent';
+import { Component } from '../../Component';
 
-export abstract class Cta {
+export abstract class Cta implements Component {
     clickEvent?: AnalyticalEvent;
     icon?: string;
     label: string;
