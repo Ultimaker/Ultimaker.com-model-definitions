f0189fa944691b2c24492ddeb92ca1fc7aed247c
Etiënne van den Tempel <e.vandentempel@ultimaker.com>
2020-02-20 09:32:16 +0100

    Add dist files

--- dist/molecules/navigation/MainNavigationItem.d.ts
+++ dist/molecules/navigation/MainNavigationItem.d.ts
@@ -1,8 +1,6 @@
 import { NavigationItem } from './NavigationItem';
-import { MainNavigationCategory } from './MainNavigationCategory';
 import { SubNavigation } from '../../organisms/sub-navigation/SubNavigation';
 import { MainNavigationCategories } from './MainNavigationCategories';
 export declare class MainNavigationItem extends NavigationItem {
-    categories?: MainNavigationCategory[];
     subNavigation?: MainNavigationCategories | SubNavigation;
 }
