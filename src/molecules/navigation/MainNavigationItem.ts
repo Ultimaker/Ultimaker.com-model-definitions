776182de9b722e6e32c0791e7158831845fab985
Etiënne van den Tempel <e.vandentempel@ultimaker.com>
2020-02-20 09:32:09 +0100

    Remove MainNavigationItem.categories

--- src/molecules/navigation/MainNavigationItem.ts
+++ src/molecules/navigation/MainNavigationItem.ts
@@ -1,9 +1,7 @@
 import { NavigationItem } from './NavigationItem';
-import { MainNavigationCategory } from './MainNavigationCategory';
 import { SubNavigation } from '../../organisms/sub-navigation/SubNavigation';
 import { MainNavigationCategories } from './MainNavigationCategories';
 
 export class MainNavigationItem extends NavigationItem {
-    categories?: MainNavigationCategory[];
     subNavigation?: MainNavigationCategories | SubNavigation;
 }
