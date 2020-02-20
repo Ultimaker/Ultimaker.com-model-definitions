a3f4db1cd88d71bd404b32e90ea6ebd63a7d2d77
dan entous <d.entous@ultimaker.com>
2020-02-20 07:40:14 +0100

    add(): Flyout, FlyoutItem, Tag models

--- src/molecules/navigation/MainNavigationItem.ts
+++ src/molecules/navigation/MainNavigationItem.ts
@@ -1,7 +1,8 @@
+import { Flyout } from '../../organisms/flyout/Flyout';
 import { NavigationItem } from './NavigationItem';
 import { SubNavigation } from '../../organisms/sub-navigation/SubNavigation';
 import { MainNavigationCategories } from './MainNavigationCategories';
 
 export class MainNavigationItem extends NavigationItem {
-    subNavigation?: MainNavigationCategories | SubNavigation;
+    subNavigation?: Flyout | MainNavigationCategories | SubNavigation;
 }
