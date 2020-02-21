2b48b1af17e1a58b8eac901cff1341aed255325e
dan entous <d.entous@ultimaker.com>
2020-02-21 09:58:30 +0100

    update(): dist

--- dist/molecules/sections/ListSection.d.ts
+++ dist/molecules/sections/ListSection.d.ts
@@ -2,16 +2,13 @@ import { Tooltip } from '../../atoms/tooltip/Tooltip';
 import { Limit } from '../../atoms/limit/Limit';
 import { BusinessCard } from '../cards/BusinessCard';
 import { ContentBlock } from '../blocks/ContentBlock';
-import { ContentButton } from '../../atoms/cta/ContentButton';
 import { CardProduct } from '../cards/CardProduct';
 import { Limitable } from '../../atoms/limit/Limitable';
-import { Expandable } from '../../atoms/limit/Expandable';
 import { Tooltippable } from '../../atoms/tooltip/Tooltippable';
 import { CardArticle } from '../cards/CardArticle';
 import { FilterGroup } from '../filter-group/FilterGroup';
-export declare class ListSection implements Limitable, Expandable, Tooltippable {
+export declare class ListSection implements Limitable, Tooltippable {
     cards: BusinessCard[] | ContentBlock[] | CardProduct[] | CardArticle[];
-    expand?: ContentButton;
     limit?: Limit;
     title?: string;
     tooltip?: Tooltip;
