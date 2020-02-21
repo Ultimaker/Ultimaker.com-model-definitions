d9e8baa128ded13103fbb91fdd7317f4ce57abab
dan entous <d.entous@ultimaker.com>
2020-02-21 09:58:08 +0100

    cleanup(): ListSection model

--- src/molecules/sections/ListSection.ts
+++ src/molecules/sections/ListSection.ts
@@ -5,15 +5,12 @@ import { ContentBlock } from '../blocks/ContentBlock';
 import { ContentButton } from '../../atoms/cta/ContentButton';
 import { CardProduct } from '../cards/CardProduct';
 import { Limitable } from '../../atoms/limit/Limitable';
-import { Expandable } from '../../atoms/limit/Expandable';
 import { Tooltippable } from '../../atoms/tooltip/Tooltippable';
 import { CardArticle } from '../cards/CardArticle';
 import { FilterGroup } from '../filter-group/FilterGroup';
 
-export class ListSection implements Limitable, Expandable, Tooltippable {
+export class ListSection implements Limitable, Tooltippable {
     cards: BusinessCard[] | ContentBlock[] | CardProduct[] | CardArticle[];
-    // @Deprecated expand will become part of limit, refactor design components
-    expand?: ContentButton;
     limit?: Limit;
     title?: string;
     tooltip?: Tooltip;
