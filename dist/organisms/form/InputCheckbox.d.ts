1d26137a641276a18083f4ef8450732c9780ea89
dan entous <d.entous@ultimaker.com>
2020-02-24 16:51:59 +0100

    update(): dist

---
+++ dist/organisms/form/InputCheckbox.d.ts
@@ -0,0 +1,3 @@
+import { Input } from './Input';
+export declare class InputCheckbox extends Input {
+}
---
+++ dist/organisms/form/InputCheckbox.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const Input_1 = require("./Input");
+class InputCheckbox extends Input_1.Input {
+}
+exports.InputCheckbox = InputCheckbox;
+//# sourceMappingURL=InputCheckbox.js.map
