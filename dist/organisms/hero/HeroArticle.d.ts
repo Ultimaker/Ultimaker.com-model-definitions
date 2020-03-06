dc8f179656cfb43a1da392422c87c640b0722f59
dan entous <d.entous@ultimaker.com>
2020-03-06 10:26:31 +0100

    update(): dist

--- dist/organisms/hero/HeroArticle.d.ts
+++ dist/organisms/hero/HeroArticle.d.ts
@@ -2,6 +2,7 @@ import { Image } from '../../atoms/media/Image';
 import { OrganismInterface } from '../OrganismInterface';
 export declare class HeroArticle implements OrganismInterface {
     title: string;
+    estimatedReadingTime?: string;
     image?: Image;
     subtitle?: string;
 }
