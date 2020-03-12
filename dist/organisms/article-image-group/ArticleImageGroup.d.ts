53e3bc03f3311b0c9fcbc408fa7e65dbda2d1d7d
dan entous <d.entous@ultimaker.com>
2020-03-12 06:57:41 +0100

    update(): dist

---
+++ dist/organisms/article-image-group/ArticleImageGroup.d.ts
@@ -0,0 +1,4 @@
+import { ArticleImage } from '../../molecules/article/ArticleImage';
+export declare class ArticleImageGroup {
+    images: ArticleImage[];
+}
---
+++ dist/organisms/article-image-group/ArticleImageGroup.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class ArticleImageGroup {
+}
+exports.ArticleImageGroup = ArticleImageGroup;
+//# sourceMappingURL=ArticleImageGroup.js.map
