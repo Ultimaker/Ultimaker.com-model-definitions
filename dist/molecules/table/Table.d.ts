a9d9a0f5822df885e45a9ca084aa5034ebcccad7
Etiënne van den Tempel <e.vandentempel@ultimaker.com>
2020-03-23 11:00:18 +0100

    Add dist files

--- dist/molecules/table/Table.d.ts
+++ dist/molecules/table/Table.d.ts
@@ -1,6 +1,7 @@
 import { TableColumn } from './TableColumn';
 import { TableRow } from './TableRow';
+import { TableRowGroup } from './TableRowGroup';
 export declare class Table {
     columns: TableColumn[];
-    rows: TableRow[];
+    rows: (TableRow | TableRowGroup)[];
 }
---
+++ dist/molecules/table/TableRowGroup.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class TableRowGroup {
+}
+exports.TableRowGroup = TableRowGroup;
+//# sourceMappingURL=TableRowGroup.js.map
