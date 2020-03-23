c11a83ec38e1b2542a0dbcc6e5e499956c6241ce
Etiënne van den Tempel <e.vandentempel@ultimaker.com>
2020-03-23 11:00:02 +0100

    Add TableRowGroup

--- src/molecules/table/Table.ts
+++ src/molecules/table/Table.ts
@@ -1,7 +1,8 @@
 import { TableColumn } from './TableColumn';
 import { TableRow } from './TableRow';
+import { TableRowGroup } from './TableRowGroup';
 
 export class Table {
     columns: TableColumn[];
-    rows: TableRow[];
+    rows: (TableRow | TableRowGroup)[];
 }
