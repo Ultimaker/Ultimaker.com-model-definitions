69f84898cd2419d742e05400a8ada8f42d31c785
dan entous <d.entous@ultimaker.com>
2020-04-06 15:24:53 +0200

    refactor(): FlyoutItem

--- src/organisms/flyout/FlyoutItem.ts
+++ src/organisms/flyout/FlyoutItem.ts
@@ -1,10 +1,12 @@
 import { Heading } from '../../atoms/heading/Heading';
+import { Link } from '../../atoms/link/Link';
 import { Paragraph } from '../../atoms/paragraph/Paragraph';
 import { Tag } from '../../atoms/tag/Tag';
 
 export class FlyoutItem {
     description?: Paragraph;
     heading: Heading;
-    url: string;
+    link: Link;
+    url: string; // @todo: remove when integration service and nuxt are deployed using link
     tag?: Tag;
 }
