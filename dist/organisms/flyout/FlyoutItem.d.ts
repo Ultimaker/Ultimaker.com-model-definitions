b78d4836ac9a70d3b827505233f8c44443fc381f
dan entous <d.entous@ultimaker.com>
2020-04-06 16:24:40 +0200

    update(): dist

--- dist/organisms/flyout/FlyoutItem.d.ts
+++ dist/organisms/flyout/FlyoutItem.d.ts
@@ -1,9 +1,11 @@
 import { Heading } from '../../atoms/heading/Heading';
+import { Link } from '../../atoms/link/Link';
 import { Paragraph } from '../../atoms/paragraph/Paragraph';
 import { Tag } from '../../atoms/tag/Tag';
 export declare class FlyoutItem {
     description?: Paragraph;
     heading: Heading;
+    link: Link;
     url: string;
     tag?: Tag;
 }
