a89de76b3d563dc494f56365ee49ad67b2a5376a
Sean de Leeuw <s.deleeuw@ultimaker.com>
2020-10-08 09:40:53 +0200

    npm run tsc

---
+++ dist/organisms/highlight-grid-row/HighlightGridRow.d.ts
@@ -0,0 +1,6 @@
+import { OrganismInterface } from '../OrganismInterface';
+import { Component } from '../../Component';
+import { ContentGeneral } from 'molecules/contentGeneral/ContentGeneral';
+export declare class InfographicJourney implements OrganismInterface, Component {
+    elements: ContentGeneral[];
+}
