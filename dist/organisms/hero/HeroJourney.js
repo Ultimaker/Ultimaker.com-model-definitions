58fee9e79833d8ba89f49c09f090db4c4d61b0f9
Etiënne <61014456+ultienne@users.noreply.github.com>
2021-05-12 09:26:05 +0200

    dist files

--- dist/organisms/hero/HeroJourney.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class HeroJourney {
-}
-exports.HeroJourney = HeroJourney;
-//# sourceMappingURL=HeroJourney.js.map
--- dist/organisms/infographic/InfographicJourney.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class InfographicJourney {
-}
-exports.InfographicJourney = InfographicJourney;
-//# sourceMappingURL=InfographicJourney.js.map
