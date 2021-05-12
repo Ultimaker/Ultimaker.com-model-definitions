340e7f449ca960e5b19920799b06024a843244e0
Etiënne <61014456+ultienne@users.noreply.github.com>
2021-05-12 09:25:56 +0200

    Remove HeroJourney / InfographicJourney

--- src/molecules/infographic/InfographicJourneyStep.ts
+++
@@ -1,6 +0,0 @@
-import { Image } from '../../atoms/media/Image';
-
-export class InfographicJourneyStep {
-    image: Image;
-    caption: string;
-}
--- src/molecules/infographic/InfographicJourneySteps.ts
+++
@@ -1,6 +0,0 @@
-import { Component } from '../../Component';
-import { InfographicJourneyStep } from './InfographicJourneyStep';
-
-export class InfographicJourneySteps implements Component {
-    steps: InfographicJourneyStep[];
-}
