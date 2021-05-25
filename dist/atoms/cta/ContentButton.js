e954c553118d521d8c1385d3990a2f204f80611f
Sean de Leeuw <s.deleeuw@ultimaker.com>
2021-05-25 15:22:26 +0200

    npm run tsc

--- dist/atoms/cta/ContentButton.js
+++ dist/atoms/cta/ContentButton.js
@@ -1,6 +1,11 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const Cta_1 = require("./Cta");
+var ButtonStyle;
+(function (ButtonStyle) {
+    ButtonStyle["primary"] = "primary";
+    ButtonStyle["secondary"] = "secondary";
+})(ButtonStyle = exports.ButtonStyle || (exports.ButtonStyle = {}));
 class ContentButton extends Cta_1.Cta {
 }
 exports.ContentButton = ContentButton;
