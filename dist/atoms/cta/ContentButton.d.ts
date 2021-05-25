a9638e89f913bbf7b4893ef4bb21947dc8b4d5b8
Sean de Leeuw <s.deleeuw@ultimaker.com>
2021-05-25 15:22:26 +0200

    npm run tsc

--- dist/atoms/cta/ContentButton.d.ts
+++ dist/atoms/cta/ContentButton.d.ts
@@ -1,5 +1,9 @@
 import { Cta } from './Cta';
+export declare enum ButtonStyle {
+    primary = "primary",
+    secondary = "secondary"
+}
 export declare class ContentButton extends Cta {
-    buttonStyle: 'primary' | 'secondary';
+    buttonStyle: ButtonStyle;
     url?: string;
 }
