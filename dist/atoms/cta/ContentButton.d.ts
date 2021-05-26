46bd10008b8cdc274ba58ac981101595c911ac41
Sean de Leeuw <s.deleeuw@ultimaker.com>
2021-05-26 14:17:38 +0200

    remove buttonStyle from contentbutton

--- dist/atoms/cta/ContentButton.d.ts
+++ dist/atoms/cta/ContentButton.d.ts
@@ -1,9 +1,4 @@
 import { Cta } from './Cta';
-export declare enum ButtonStyle {
-    primary = "primary",
-    secondary = "secondary"
-}
 export declare class ContentButton extends Cta {
-    buttonStyle: ButtonStyle;
     url?: string;
 }
