7e9e2a740cad2063d850c98308b98e5d9e77ac71
Sean de Leeuw <s.deleeuw@ultimaker.com>
2021-05-31 11:52:58 +0200

    add buttonStyle to contentbutton

--- src/atoms/cta/ContentButton.ts
+++ src/atoms/cta/ContentButton.ts
@@ -1,5 +1,11 @@
 import { Cta } from './Cta';
 
+export enum ButtonStyle {
+    primary = 'primary',
+    secondary = 'secondary',
+}
+
 export class ContentButton extends Cta {
+    buttonStyle?: ButtonStyle;
     url?: string;
 }
