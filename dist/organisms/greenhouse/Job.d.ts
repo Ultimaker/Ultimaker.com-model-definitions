a8a078044fbf39f290998554db1fd39259a29709
Sean de Leeuw <s.deleeuw@ultimaker.com>
2021-06-16 11:24:06 +0200

    remove Department, Job, JobBoard, JobBoardFrame

--- dist/organisms/greenhouse/Job.d.ts
+++
@@ -1,8 +0,0 @@
-import { OrganismInterface } from '../OrganismInterface';
-export declare class Job implements OrganismInterface {
-    id: number;
-    title: string;
-    location: string;
-    htmlContent?: string;
-    detailPageUrl?: string;
-}
--- src/organisms/greenhouse/JobBoardFrame.ts
+++
@@ -1,4 +0,0 @@
-import { OrganismInterface } from '../OrganismInterface';
-
-export class JobBoardFrame implements OrganismInterface {
-}
